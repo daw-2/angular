de32c142eb03704756089ca30d1b0cd0449b7865
Matthieu Mota <matthieumota@gmail.com>
2019-11-19 09:21:34 +0100

    Prepare design app

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
   isCollapsed: boolean = false;
+  @Input() title: string;
 
   constructor() { }
 
